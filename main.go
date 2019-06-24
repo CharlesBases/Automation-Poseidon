3b2235821a1268a7194cc707b974f32bec67c0b8
sunzhiming <1162776833@qq.com>
2019-06-24 14:45:54 +0800

    rpc

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 var (
 	goFile       = flag.String("file", "", "full path of the file")
 	generatePath = flag.String("path", "./pb/", "full path of the generate folder")
-	protoPackage = flag.String("package", "auto", "package name in .proto file")
+	protoPackage = flag.String("package", "", "package name in .proto file")
 )
 
 var (
